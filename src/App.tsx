48b06cc4faa1fd825babbc2a1decca4453ba7654
Sreetam Das <sreetamdas@gmail.com>
2022-06-04 09:42:35 +0530

    feat: import MDX successfully, proof of concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,15 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { MDXProvider } from "@mdx-js/react";
+import Deck from "./deck.mdx";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+	const components = {};
+	return (
+		<div className="App">
+			<MDXProvider components={components}>
+				<Deck />
+			</MDXProvider>
+		</div>
+	);
+};
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
-
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import mdx from "@mdx-js/rollup";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+	optimizeDeps: {
+		include: ["react/jsx-runtime"],
+	},
+	plugins: [mdx({}), react()],
+});
