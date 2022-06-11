4299503b320a5ab08e010f83f56ccf60969d9791
Sreetam Das <sreetamdas@gmail.com>
2022-06-11 23:53:46 +0530

    improvement: create fresh types on build

--- packages/mdx-frisson/src/main.ts
+++ packages/mdx-frisson/src/main.ts
@@ -1,7 +1,5 @@
 import { CompileOptions as MDXCompileOptions, compile } from "@mdx-js/mdx";
 
-export function MDXDeck() {}
-
 type CompileOptions = MDXCompileOptions & Required<{ baseUrl: string }>;
 export async function bundleMDXDeck(rawInput: string, options: CompileOptions) {
 	const result = await compile(rawInput, {
