457a65c3d0d9bee7f69132a17c7965636cc35020
Sreetam Das <sreetamdas@gmail.com>
2022-06-16 01:27:09 +0530

    style: update styles to wrap nodes/pages

--- packages/mdx-frisson-starter/src/components/index.tsx
+++ packages/mdx-frisson-starter/src/components/index.tsx
@@ -1,5 +1,15 @@
 import { PropsWithChildren } from "react";
 
 export const Highlight = ({ children }: PropsWithChildren) => {
-	return <h3 style={{ color: "#5b34da", fontSize: "3rem" }}>{children}</h3>;
+	return <span style={{ color: "#5b34da", fontSize: "2rem" }}>{children}</span>;
+};
+
+export const Layout = ({ children }: PropsWithChildren) => {
+	return (
+		<div
+			style={{ border: "2px solid #5b34da", margin: "10px", padding: "0 15px" }}
+		>
+			{children}
+		</div>
+	);
 };
